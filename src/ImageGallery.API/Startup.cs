df2de127ff4a2d18cc81690e8f65739cfd547fe9
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-03 16:55:18 -0600

    Added custom MustOwnImage Authorization rule.

--- src/ImageGallery.API/Startup.cs
+++ src/ImageGallery.API/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using IdentityServer4.AccessTokenValidation;
+using ImageGallery.API.Authorization;
 using ImageGallery.API.Entities;
 using ImageGallery.API.Services;
 using ImageGallery.Common.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -23,6 +25,19 @@ namespace ImageGallery.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(
+                    "MustOwnImage",
+                    policyBuilder =>
+                    {
+                        policyBuilder.RequireAuthenticatedUser();
+                        policyBuilder.AddRequirements(new MustOwnImageRequirement());
+                    });
+            });
+
+            services.AddScoped<IAuthorizationHandler, MustOwnImageHandler>()
 
             services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
                 .AddIdentityServerAuthentication(options =>
