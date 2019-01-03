2638597bfbf14ef44a53765ebf5b6641fb8fb3b4
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-03 13:49:56 -0600

    Added code to include a Bearer token in API calls.

--- src/ImageGallery.Client/Services/ImageGalleryHttpClient.cs
+++ src/ImageGallery.Client/Services/ImageGalleryHttpClient.cs
@@ -3,6 +3,8 @@ using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace ImageGallery.Client.Services
 {
@@ -11,13 +13,21 @@ namespace ImageGallery.Client.Services
         private readonly IHttpContextAccessor _httpContextAccessor;
         private HttpClient _httpClient = new HttpClient();
 
-        public ImageGalleryHttpClient(IHttpContextAccessor httpContextAccessor)
-        {
-            _httpContextAccessor = httpContextAccessor;
-        }
-        
+        public ImageGalleryHttpClient(IHttpContextAccessor httpContextAccessor) => _httpContextAccessor = httpContextAccessor;
+
         public async Task<HttpClient> GetClient()
-        {      
+        {
+            var accessToken = string.Empty;
+
+            var currentContext = _httpContextAccessor.HttpContext;
+
+            accessToken = await currentContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            if (!string.IsNullOrWhiteSpace(accessToken))
+            {
+                _httpClient.SetBearerToken(accessToken);
+            }
+
             _httpClient.BaseAddress = new Uri("https://localhost:44308/");
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(
