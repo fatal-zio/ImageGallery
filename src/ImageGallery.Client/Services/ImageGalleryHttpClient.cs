0cfa8bfff54c5da2475a236395940a05335e85c5
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-04 11:07:16 -0600

    Removed Token Refresh logic for simplicity.

--- src/ImageGallery.Client/Services/ImageGalleryHttpClient.cs
+++ src/ImageGallery.Client/Services/ImageGalleryHttpClient.cs
@@ -24,18 +24,8 @@ namespace ImageGallery.Client.Services
 
             var currentContext = _httpContextAccessor.HttpContext;
 
-            var expiresAt = await currentContext.GetTokenAsync("expires_at");
-
-            if (string.IsNullOrWhiteSpace(expiresAt) ||
-                (DateTime.Parse(expiresAt).AddSeconds(-60).ToUniversalTime() < DateTime.UtcNow))
-            {
-                accessToken = await RenewTokens();
-            }
-            else
-            {
-                accessToken = await currentContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
-            }
-
+            accessToken = await currentContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            
             if (!string.IsNullOrWhiteSpace(accessToken))
             {
                 _httpClient.SetBearerToken(accessToken);
@@ -47,65 +37,6 @@ namespace ImageGallery.Client.Services
                 new MediaTypeWithQualityHeaderValue("application/json"));
 
             return _httpClient;
-        }
-
-        private async Task<string> RenewTokens()
-        {
-            var currentContext = _httpContextAccessor.HttpContext;
-
-            var discoveryClient = new DiscoveryClient("https://localhost:44344/");
-            var metaDataResponse = await discoveryClient.GetAsync();
-
-            var tokenClient = new TokenClient(metaDataResponse.TokenEndpoint, "imagegalleryclient", "secret");
-
-            var currentRefreshToken = await currentContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
-
-            var tokenResult = await tokenClient.RequestRefreshTokenAsync(currentRefreshToken);
-
-            if (!tokenResult.IsError)
-            {
-                var updatedTokens = new List<AuthenticationToken>
-                {
-                    new AuthenticationToken()
-                    {
-                        Name = OpenIdConnectParameterNames.IdToken,
-                        Value = tokenResult.IdentityToken
-                    },
-
-                    new AuthenticationToken()
-                    {
-                        Name = OpenIdConnectParameterNames.AccessToken,
-                        Value = tokenResult.AccessToken
-                    },
-
-                    new AuthenticationToken()
-                    {
-                        Name = OpenIdConnectParameterNames.RefreshToken,
-                        Value = tokenResult.RefreshToken
-                    }
-                };
-
-                var expiresAt = DateTime.UtcNow + TimeSpan.FromSeconds(tokenResult.ExpiresIn);
-
-                updatedTokens.Add(new AuthenticationToken()
-                {
-                    Name = "expires_at",
-                    Value = expiresAt.ToString("o", CultureInfo.InvariantCulture)
-                });
-
-                var currentAuthenticateResult = await currentContext.AuthenticateAsync("Cookies");
-
-                currentAuthenticateResult.Properties.StoreTokens(updatedTokens);
-
-                await currentContext.SignInAsync("Cookies", currentAuthenticateResult.Principal,
-                    currentAuthenticateResult.Properties);
-
-                return tokenResult.AccessToken;
-            }
-            else
-            {
-                throw new Exception("Problem encountered while refreshing tokens.", tokenResult.Exception);
-            }
         }
     }
 }
